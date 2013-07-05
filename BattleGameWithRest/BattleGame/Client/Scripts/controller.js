ef5623e1fba08f8139031758dabac28897c761ae
tvetan <tvetan@gbg.bg>
2013-07-05 21:15:39 +0300

    Adding Battle Game

---
+++ BattleGameWithRest/BattleGame/Client/Scripts/controller.js
@@ -0,0 +1,379 @@
+﻿/// <reference path="class.js" />
+/// <reference path="persister.js" />
+/// <reference path="jquery-2.0.2.js" />
+/// <reference path="ui.js" />
+
+var controllers = (function () {
+    var rootUrl = "http://localhost:22954/api/";
+    var Controller = Class.create({
+        init: function () {
+            this.persister = persisters.get(rootUrl);
+            this.updater = null;
+            this.interval = 25000;
+        },
+        renderGameUI: function (selector) {
+            var self = this;
+            this.updater = setInterval(function () {
+
+                if (self.persister.isUserLoggedIn()) {
+                    self.loadActiveGames(selector);
+                    self.loadOpenGames(selector)
+                }
+
+                if (self.isGameActive()) {
+                    var gameId = localStorage.getItem("active-game");
+                    console.log("here")
+                   self.loadGame(selector, gameId)
+                }
+
+
+            }, this.interval)
+        },
+        loadUI: function (selector) {
+            localStorage.removeItem("active-game")
+            if (this.persister.isUserLoggedIn()) {
+                this.loadGameUI(selector);
+                this.renderGameUI(selector)
+            }
+            else {
+                this.loadLoginFormUI(selector);
+            }
+            this.attachUIEventHandlers(selector);
+        },
+        loadLoginFormUI: function (selector) {
+            $("#messages").html("")
+            var loginFormHtml = ui.loginForm()
+            $(selector).html(loginFormHtml);
+        },
+        loadGameUI: function (selector) {
+            console.log(selector)
+            var list =
+            ui.gameUI(this.persister.nickname());
+            $(selector).html(list);
+
+            this.loadOpenGames(selector)
+
+            this.loadActiveGames(selector)
+
+            this.loadMessages("#messages")
+        },
+        loadGame: function (selector, gameId) {
+            this.persister.game.state(gameId, function (gameState) {
+                var gameHtml = ui.gameState(gameState);
+
+                $(selector + " #game-holder").html(gameHtml)
+                ui.fillEmptyTable(gameState)
+            }, function (error) {
+                console.log(error)
+                var errorType = JSON.parse(error.responseText).Message;
+                $("#game-holder").append("<p class='error'>"+errorType+"</p>")
+            });
+        },
+        loadActiveGames: function (selector) {
+            this.persister.game.myActive(function (games) {
+                var list = ui.activeGamesList(games);
+                $(selector + " #active-games")
+                .html(list);
+            });
+        },
+        loadOpenGames : function (selector) {
+            this.persister.game.open(function (games) {
+                var list = ui.openGamesList(games);
+                $(selector + " #open-games")
+                .html(list);
+            }, function (error) {
+                console.log(error)
+            });
+        },
+        loadUnreadMessages: function(selector){
+            this.persister.messages.unread(function (messages) {
+                var list = ui.showMessages(messages);
+                $(selector + " #message-list")
+                .prepend(list);
+            }, function (error) {
+                console.log(error);
+                console.log("Invalid user authentication - The only posible error with messages");
+            })
+        },
+        loadMessages: function (selector) {
+            this.persister.messages.all(function (messages) {
+                var list = ui.showMessages(messages);
+
+                $(selector)
+                .html(list);
+            }, function (error) {
+                console.log(error);
+                console.log("Invalid user authentication - The only posible error with messages");
+            })
+        },
+        gameInActiveGames: function (gameId) {
+            console.log(this)
+            this.persister.game.myActive(function (data) {
+                var mine = JSON.stringify(data);
+                console.log(mine.length)
+                console.log(gameId)
+                for (var i in mine) {
+                    console.log(i)
+                    if (i == gameId) {
+                        return ;
+                    }
+                }
+                
+                $("#game-state #error-block").html("<h2 id='error'>You won the game</h2>")
+                return false;
+            })
+        },
+
+        escapeHtml: function(text){
+            var entityMap = {
+                "&": "&amp;",
+                "<": "&lt;",
+                ">": "&gt;",
+                '"': '&quot;',
+                "'": '&#39;',
+                "/": '&#x2F;'
+            };
+
+           
+            return String(string).replace(/[&<>"'\/]/g, function (s) {
+                return entityMap[s];
+            });
+        },
+        isGameActive: function () {    
+            return localStorage.getItem("active-game") !=null;
+        },
+
+        attachUIEventHandlers: function (selector) {
+            var wrapper = $(selector);
+            var self = this;
+
+            wrapper.on("click", "#btn-show-login", function () {
+                wrapper.find(".button.selected").removeClass("selected");
+                $(this).addClass("selected");
+                wrapper.find("#login-form").show();
+                wrapper.find("#register-form").hide();
+            });
+            wrapper.on("click", "#btn-show-register", function () {
+                wrapper.find(".button.selected").removeClass("selected");
+                $(this).addClass("selected");
+                wrapper.find("#register-form").show();
+                wrapper.find("#login-form").hide();
+            });
+
+            wrapper.on("click", "#btn-login", function () {
+                var user = {
+                    username: $(selector + " #tb-login-username").val(),
+                    password: $(selector + " #tb-login-password").val()
+                }
+                localStorage.removeItem("active-game")
+                self.persister.user.login(user, function (data) {
+                    self.loadGameUI(selector);
+                }, function (error) {
+                    console.log(error)
+                    var errorType = JSON.parse(error.responseText).Message;
+                    console.log(errorType);
+                    //var errorText = self.filterErrorsLogin(errorType)
+                   
+                    wrapper.append("<div class='invalid-user-information'>"+ errorType +"</div>");
+                });
+
+                return false;
+            });
+            wrapper.on("click", "#btn-register", function () {
+                var user = {
+                    username: $(selector + " #tb-register-username").val(),
+                    nickname: $(selector + " #tb-register-nickname").val(),
+                    password: $(selector + " #tb-register-password").val()
+                }
+
+                self.persister.user.register(user, function (data) {
+                    console.log(selector)
+                    self.loadGameUI(selector)
+                }, function (error) {
+                    console.log(error);
+                    wrapper.html(error.responseText)
+                })
+
+                return false;
+            });
+            wrapper.on("click", "#btn-logout", function () {
+                self.persister.user.logout(function () {
+                    self.loadLoginFormUI(selector);
+                });
+                localStorage.removeItem("active-game")
+            });
+
+            wrapper.on("click", "#open-games-container a", function () {
+                $("#game-join-inputs").remove();
+                var html =
+                '<div id="game-join-inputs">' +
+                //'Number: <input type="text" id="tb-game-number"/>' +
+                'Password: <input type="text" id="tb-game-pass"/>' +
+                '<button id="btn-join-game">join</button>' +
+                '</div>';
+                $(this).after(html);
+            });
+            wrapper.on("click", "#btn-join-game", function () {
+                var game = {
+                    number: $("#tb-game-number").val(),
+                    gameId: $(this).parents("li").first().data("game-id")
+                };
+
+                var password = $("#tb-game-pass").val();
+
+                if (password) {
+                    game.password = password;
+                }
+                self.persister.game.join(game, function (data) {
+                    self.loadActiveGames(selector);
+                    self.loadUnreadMessages("#messages");
+                    $(selector + " #game-join-inputs").remove();
+                }, function (error) {
+                    console.log(error)
+                });
+            });
+            wrapper.on("click", "#btn-create-game", function () {
+                var game = {
+                    title: $("#tb-create-title").val(),
+                    //number: $("#tb-create-number").val(),
+                }
+                var password = $("#tb-create-pass").val();
+                if (password) {
+                    game.password = password;
+                }
+                self.persister.game.create(game, function () {
+                    self.loadActiveGames(selector)
+                    $("#create-game-holder .error-game-create").html("")
+                }, function (error) {
+                    var errorType = JSON.parse(error.responseText).Message;
+                    $("#create-game-holder .error-game-create").html(errorType)
+                    console.log(error)
+                });
+
+                return false;
+            });
+
+            wrapper.on("click", ".active-games .in-progress", function () {
+                self.loadGame(selector, $(this).parent().data("game-id"));
+                localStorage.setItem("active-game", $(this).parent().data("game-id"))
+            });
+
+            wrapper.on("click", ".active-games .full", function () {
+                self.persister.game.start($(this).parent().data("game-id"), function (data) {
+                    self.loadActiveGames(selector)
+                }, function (error) {
+                    console.log(error)
+                });
+            })
+
+            wrapper.on("click", "#game-state #btn-guess-number", function () {
+                var gameId = $(this).parent().parent().data("game-id");
+                
+                self.persister.guess.make($("#guessed-number").val(), $(this).parent().parent().data("game-id"), function (data) {
+                    self.loadGame(selector, gameId)
+                }, function (error) {
+                    console.log("here")
+                    if (JSON.parse(error.responseText).errCode == "INV_OP_TURN") {
+                        $("#game-state #error-block").html("<h2 id='error'>Sorry but it is not your turn</h2>")
+                    }
+                    else {
+                        console.log(error)
+                    }
+                })
+            })
+
+            wrapper.on("click", "#game-state #btn-move-unit", function () {
+                var input = ui.moveInputs;
+                $(selector + " #command-inputs").html(input)
+            })
+
+            wrapper.on("click", "#game-state #btn-attack-unit", function () {
+                
+                var input = ui.attackInputs;
+                $(selector + " #command-inputs").html(input)
+            })
+
+            wrapper.on("click", "#game-state #btn-defend-unit", function () {
+                var input = ui.defendInputs;
+                $(selector + " #command-inputs").html(input)
+            })
+
+            // commands
+            wrapper.on("click", "#game-state #btn-move-command", function () {
+                
+                var moveTarget = $(selector + " #target-to-move").val();
+                var gameId = localStorage.getItem("active-game");
+                var unitId = parseInt($("#" + moveTarget).attr("data-unit-id"));
+
+                var positionToMove = $(selector + " #position-to-move").val();
+
+                var position = {
+                    x: positionToMove[0],
+                    y: positionToMove[2]
+                }
+                
+                //gameId, unitId, position, success, error
+                self.persister.battle.move(gameId, unitId,position,function () {
+                    self.loadGame(selector,gameId)
+                }, function (error) {
+                    var errorType = JSON.parse(error.responseText).Message;
+                    $(selector + " #error-block").text(errorType)
+                })
+            })
+
+            wrapper.on("click", "#game-state #btn-attack-command", function () {
+
+                var moveTarget = $(selector + " #target-to-attack").val();
+                var gameId = localStorage.getItem("active-game");
+                var unitId = parseInt($("#" + moveTarget).attr("data-unit-id"));
+
+                var positionToMove = $(selector + " #position-to-attack").val();
+
+                var position = {
+                    x: positionToMove[0],
+                    y: positionToMove[2]
+                }
+
+                //gameId, unitId, position, success, error
+                self.persister.battle.move(gameId, unitId, position, function () {
+                    self.loadGame(selector, gameId)
+                }, function (error) {
+                    var errorType = JSON.parse(error.responseText).Message;
+                    $(selector + " #error-block").text(errorType)
+                })
+
+                //gameId, unitId, position, success, error
+                self.persister.battle.attack(gameId, unitId, position, function () {
+
+                }, function (error) {
+                    var errorType = JSON.parse(error.responseText).Message;
+                    $(selector + " #error-block").text(errorType)
+                })
+            })
+
+            wrapper.on("click", "#game-state #btn-defend-command", function () {
+                
+                var deffendId = $(selector + " #target-to-defend").val();
+                var unitId = parseInt($("#" + deffendId).attr("data-unit-id"));
+                var gameId = localStorage.getItem("active-game");
+                self.persister.battle.defend(gameId, unitId, function () {
+                    console.log("we defended")
+                }, function (error) {
+                    var errorType = JSON.parse(error.responseText).Message;
+                    $(selector +" #error-block").text(errorType)
+                })
+            })
+
+        }
+    });
+    return {
+        get: function () {
+            return new Controller();
+        }
+    }
+}());
+
+$(function () {
+    var controller = controllers.get();
+    controller.loadUI("#content");
+});
---
+++ BattleGameWithRest/BattleGame/Client/Scripts/ui.js
@@ -0,0 +1,271 @@
+﻿/// <reference path="jquery-2.0.2.js" />
+var ui = (function () {
+
+	function buildLoginForm() {
+		var html =
+            '<div id="login-form-holder">' +
+				'<form>' +
+					'<div id="login-form">' +
+						'<label for="tb-login-username">Username: </label>' +
+						'<input type="text" id="tb-login-username"><br />' +
+						'<label for="tb-login-password">Password: </label>' +
+						'<input type="password" id="tb-login-password"><br />' +
+						'<button id="btn-login" class="button">Login</button>' +
+					'</div>' +
+					'<div id="register-form" style="display: none">' +
+						'<label for="tb-register-username">Username: </label>' +
+						'<input type="text" id="tb-register-username"><br />' +
+						'<label for="tb-register-nickname">Nickname: </label>' +
+						'<input type="text" id="tb-register-nickname"><br />' +
+						'<label for="tb-register-password">Password: </label>' +
+						'<input type="password" id="tb-register-password"><br />' +
+						'<button id="btn-register" class="button">Register</button>' +
+					'</div>' +
+					'<a href="#" id="btn-show-login" class="button selected">Login</a>' +
+					'<a href="#" id="btn-show-register" class="button">Register</a>' +
+				'</form>' +
+            '</div>';
+		return html;
+	}
+
+	function buildGameUI(nickname) {
+		var html = '<span id="user-nickname">' +
+				nickname +
+		'</span>' +
+		'<button id="btn-logout">Logout</button><br/>' +
+		'<div id="create-game-holder">' +
+			'Title: <input type="text" id="tb-create-title" />' +
+			'Password: <input type="text" id="tb-create-pass" />' +
+			'<button id="btn-create-game">Create</button>' +
+            '<div class="error-game-create"></div>'+
+		'</div>' +
+		'<div id="open-games-container">' +
+			'<h2>Open</h2>' +
+			'<div id="open-games"></div>' +
+		'</div>' +
+		'<div id="active-games-container">' +
+			'<h2>Active</h2>' +
+			'<div id="active-games"></div>' +
+		'</div>' +
+		'<div id="game-holder">' +
+		'</div>';
+		return html;
+	}
+
+	function buildOpenGamesList(games) {
+		var list = '<ul class="game-list open-games">';
+		for (var i = 0; i < games.length; i++) {
+			var game = games[i];
+			list +=
+				'<li data-game-id="' + game.id + '">' +
+					'<a href="#" >' +
+						$("<div />").html(game.title).text() +
+					'</a>' +
+					'<span> by ' +
+						game.creator +
+					'</span>' +
+				'</li>';
+		}
+		list += "</ul>";
+		return list;
+	}
+
+	function buildMessageList(messages) {
+	    var list = '<h1>Messages</h1> <ul class="game-list open-games">';
+	    for (var i = 0; i < messages.length; i++) {
+	        var message = messages[i];
+           // console.log(message)
+	        list +=
+				'<li data-game-message-id="' + message.gameId + '">' +
+					'<div  >' +
+						message.text +
+					'</div>' +
+					'<span>  ' +
+						message.type +
+					'</span>' +
+				'</li>';
+	    }
+	    list += "</ul>";
+	    return list;
+	}
+
+	function buildActiveGamesList(games) {
+		var gamesList = Array.prototype.slice.call(games, 0);
+		gamesList.sort(function (g1, g2) {
+			if (g1.status == g2.status) {
+				return g1.title > g2.title;
+			}
+			else
+			{
+				if (g1.status == "in-progress") {
+					return -1;
+				}
+			}
+			return 1;
+		});
+
+		var list = '<ul class="game-list active-games">';
+		for (var i = 0; i < gamesList.length; i++) {
+			var game = gamesList[i];
+			list +=
+				'<li data-game-id="' + game.id + '">' +
+					'<a href="#" class="' + game.status + '">' +
+						$("<div />").html(game.title).text() +
+					'</a>' +
+					'<span> by ' +
+						game.creator +
+					'</span>' +
+				'</li>';
+		}
+		list += "</ul>";
+		return list;
+	}
+
+	function buildGuessTable(guesses) {
+		var tableHtml =
+			'<table border="1" cellspacing="0" cellpadding="5">' +
+				'<tr>' +
+					'<th>Number</th>' +
+					'<th>Cows</th>' +
+					'<th>Bulls</th>' +
+				'</tr>';
+		for (var i = 0; i < guesses.length; i++) {
+			var guess = guesses[i];
+			tableHtml +=
+				'<tr>' +
+					'<td>' +
+						guess.number +
+					'</td>' +
+					'<td>' +
+						guess.cows+
+					'</td>' +
+					'<td>' +
+						guess.bulls+
+					'</td>' +
+				'</tr>';
+		}
+		tableHtml += '</table>';
+		return tableHtml;
+	}
+
+	function makeEmptyTable() {
+	    var tableData = $("<table />");
+	    for (var row = 0; row < 9; row++) {
+	        var tr = $("<tr/>");
+	        for (var column = 0; column < 9; column++) {
+                var td = $("<td/>").attr("id", row + "-" + column)
+                tr.append(td)
+	        }
+            tableData.append(tr)
+	    }
+        console.log(tableData.html())
+        return tableData.html()
+	}
+
+	function fillEmptyTable( gameState) {
+	    var blueUnits = gameState.blue.units;
+	    var redUnits = gameState.red.units;
+
+	    for (var index = 0; index < blueUnits.length; index++) {
+	        var unitId = blueUnits[index].id;
+	        var positionInTD = blueUnits[index].position.x + "-" + blueUnits[index].position.y;
+	        var unitType = blueUnits[index].type == "warrior" ? "w" : "r"
+	        console.log("#" + positionInTD)
+	        $("#" + positionInTD).attr("data-unit-id", unitId);
+	        console.log()
+	        
+	        $("#" + positionInTD).text(unitType).addClass(blueUnits[index].type + "-blue")
+	    }
+
+	    for (var index = 0; index < blueUnits.length; index++) {
+	        var unitIdRed = redUnits[index].id;
+	        var positionInTDRed = redUnits[index].position.x + "-" + redUnits[index].position.y;
+	        var unitTypeRed = redUnits[index].type == "warrior" ? "w" : "r"
+	        $( "#" + positionInTDRed).attr("data-unit-id", unitIdRed);
+	        $(" #" + positionInTDRed).text(unitTypeRed).addClass(blueUnits[index].type+"-red")
+	    }
+
+	    console.log(blueUnits)
+	    console.log(redUnits)
+	}
+
+	function buildGameState(gameState) {
+
+	    var userInturn = gameState.inTurn == "blue" ? gameState.blue.nickname : gameState.red.nickname;
+	    var emptyTable = makeEmptyTable();
+        console.log()
+		var html =
+			'<div id="game-state" data-game-id="' + gameState.gameId + '">' +
+				'<h2>' + gameState.title + " " + userInturn +"'s turn"+ '</h2>' +
+				'<div  class="guess-holder">' +
+					'<h3>' +
+					    '<span id="blue-guesses">' +	gameState.blue.nickname +'</span>'+ ' VS '+  '<span id="red-guesses">' +
+                        gameState.red.nickname +'</span>' +
+					'</h3>'+
+				'</div>' +
+				
+                '<div id="player-in-turn-' + gameState.inTurn + '">' + 
+        '</div>' +
+            '<div id="game-commands">' + 
+                '<button id="btn-move-unit">Move inputs</button>' + 
+                '<button id="btn-attack-unit">Attack inputs</button>' +
+                '<button id="btn-defend-unit">Defend inputs</button>' +
+            '</div>' +
+            '<div id="command-inputs">' + '</div>' +
+          '<table id="field">' + emptyTable + '</table>' +
+
+            '<div id="error-block">' + '</div>'
+		'</div>';
+		return html;
+	}
+
+	function moveInputs() {
+
+	    var html = '<label>Target Position (1-1):</label>' +
+                    '<input type="text" value="" id="target-to-move" />'+
+                    '<br />'+
+                    '<label> Postion to move</label>'+
+                    '<input type="text" value="" id="position-to-move" />' +
+                    '<button id="btn-move-command">Move</button>'
+
+
+	    return html;
+	}
+
+	function defendInputs() {
+
+	    var html = '<label>Defend Position (1-1):</label>' +
+                    '<input type="text" value="" id="target-to-defend" />' +
+                    '<button id="btn-defend-command">Defend</button>'
+	        ;
+                    
+	    return html;
+	}
+
+	function attackInputs() {
+
+	    var html = '<label>Target Position (1-1):</label>' +
+                    '<input type="text" value="" id="target-to-attack" />' +
+                    '<br />' +
+                    '<label> Postion to attack</label>' +
+                    '<input type="text" value="" id="position-to-attack" />'  +
+                    
+                '<button id="btn-attack-command">Attack</button>'
+
+	    return html;
+	}
+
+	return {
+		gameUI: buildGameUI,
+		openGamesList: buildOpenGamesList,
+		loginForm: buildLoginForm,
+		activeGamesList: buildActiveGamesList,
+		showMessages:buildMessageList,
+		gameState: buildGameState,
+		fillEmptyTable: fillEmptyTable,
+		moveInputs: moveInputs,
+		defendInputs: defendInputs,
+        attackInputs: attackInputs
+	}
+
+}());
